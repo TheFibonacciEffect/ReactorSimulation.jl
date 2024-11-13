c7d7b701c9839176c6817fa22b55dc5636e96248
Caspar Gutsche <caspargutsche1@gmail.com>
2024-11-13 10:55:08 +0100

    set b=0 for testing, implement beta and inner part

--- src/ex3.jl
+++ src/ex3.jl
@@ -13,13 +13,9 @@ D_slow = 0.16
 νΣf_s = 0.069
 Σ12 = 0.038
 a = 50
-b = 10
+b = 0 # Set to 0 for testing
 
 round5(x) = round(x, digits=5)
-function beta(i,j)
-    return D
-end
-
 function calculate_k(a,d)
     # thermal diffusion length squared, L_th^2
     L_th_sq = D / Σa
@@ -43,10 +39,8 @@ function apply_boundary_conditions!(A, D, dx, n)
     # nothing to do because boundary is 0
 end
 
-get_buckling() = π/(2a + 2*extr_l)
-
 function analytical_reactor_without_reflector(x)
-    B = π/(2a + 2*extr_l)
+    B = π/(2a)
     cos(B*x)
 end
 
@@ -64,12 +58,11 @@ function left_side(D,Σa,n, dx)
 end
 
 function reactor_without_reflector(dx; save = false, do_plot=false, verbose=false, max=false)
-    println("------- start run ------")
+    println("------- start run for bare reactor ------")
     # numerical Parameters
-    l = 2a + 2extr_l
+    l = 2a
     n = l ÷ dx -1 |> Int
     x =  range(-l/2, l/2,n)
-    B = get_buckling() # assume fast and slow buckling to be the same
     # slow neutrons right hand side
     diffusion_slow = left_side(D_slow, Σa_s, n, dx)
     # fast neutrons right hand side
@@ -99,33 +92,77 @@ function reactor_without_reflector(dx; save = false, do_plot=false, verbose=fals
     phi = phi ./ phi[n ÷ 2]
     p1 = plot(x,phi[1:n], label="fast neutrons")
     plot!(x,phi[n+1:end], label="slow neutrons")
-    
-    # reactor_length = n
-    # fission = νΣf * spdiagm(0 => ones(reactor_length))
-    # F = fission
-    # apply_boundary_conditions!(M,D,dx,n)
-    # # initial guesses
-    # k = 1
-    # P = ones(n)
-    # P = jacobi_iteration!(M,F,P,k)
-    # Pl, Pr = check_boundary(P,dx)
-    # p1 = plot(x,P, label="numerical")
-    # plot!(x,analytical_reactor_without_reflector.(x), label="analytical")
-    # p_err = plot(x, P .- analytical_reactor_without_reflector.(x), label="error")
-    # p_boundary_conditions = plot(x[2:end],Pl,title="Boundary Conditions")
-    # plot!(x[2:end],Pr)
-    # @show P[1], P[end]
-    # @show P[Int((1.05 + l/2) ÷ ustrip(dx))] - 9.9723e-1
-    # @show analytical_reactor_without_reflector(1.05) - 9.9723e-1
-    # JL = (P[2] - P[1])/dx
-    # JR = (P[end-1] - P[end])/dx
-    # @show calculate_k(a,extr_l) |> round5
-    # println("Flux at the Boundary")
-    # analytical_reactor_without_reflector(-a/2) |> round5
-    # analytical_reactor_without_reflector(a/2) |> round5
-    # plot(p1,p_err)
 end
-# reactor_without_reflector(0.1)
-
 reactor_without_reflector(0.1)
+savefig("docs/figs/ex3/bare.png")
+
+# functions for reactor with reflector
+
+function beta(i,D)
+    # im using a beta without dx here
+    # for i+1 it would be
+    # return 2 * Di *Dip1/(Dip1+Di)
+    return 2*D[i-1] * D[i] / (D[i] + D[i-1])
+end
+
+
+function apply_inner!(A,n,dx,D, Σa)
+    for i = 2:n-1
+        A[i,i] = beta(i+1,D)/dx^2 + beta(i-1,D)/dx^2 + Σa[i]
+        A[i,i-1] = - beta(i-1,D)/dx^2
+        A[i,i+1] = -beta(i+1,D) / dx^2
+    end
+end
+
+function left_side(D::Array,Σa::Array,n, dx)
+    # TODO
+    # maybe easiest to make a for loop to fill up the matrix, instead of thinking how to combine the vector and the matrix
+    laplace = spzeros(n,n)
+    collision = - Σa * I
+    
+    M = streaming + collision
+end
+
+function reactor_with_reflector(dx; save = false, do_plot=false, verbose=false, max=false)
+    println("------- start run for reactor with reflector ------")
+    # numerical Parameters
+    l = 2a + 2b
+    nc = 2a ÷ dx -1 |> Int
+    nr = b ÷ dx -1 |> Int
+    x =  range(-l/2, l/2,nc+ 2nr)
+    # slow neutrons right hand side
+    diffusion_slow = left_side(D_slow, Σa_s, nc, dx)
+    # fast neutrons right hand side
+    diffusion_fast = left_side(D_fast, Σa_f, nc, dx)
+    # Assume Φf; Φs
+    rector_reflector_boundary = spzeros(nr, nc)
+    rector_reflector_boundary[end,1] = -D/2 # or something like that
+    # A = [
+    #     diffusion_slow_reflector    reactor_reflector_boundary spzeros(nr,nc)
+    #     reactor_reflector_boundary'    diffusion_fast-Σ12 *I   spzeros(nc,nc)
+    #     reactor_reflector_boundary'     spzeros(nc,nc)
+    #     spzeros(nr,nc)                + Σ12 * I               diffusion_slow 
+    # ]
+    println("A")
+    display(A)
+    # fast, slow
+    # the minus is important
+    F = - [
+        νΣf_f * I νΣf_s * I
+        spzeros(nc,nc) spzeros(nc,nc)
+    ]
+    println("F")
+    display(F)
+    # make sparse matrecies dense
+    A = Matrix(A)
+    F = Matrix(F)
+    M = inv(A) * F
+    @show k = eigvals(M)[end]
+    phi = eigvecs(M)[:,end]
+    phi = real.(phi)
+    phi = phi ./ phi[nc ÷ 2]
+    p1 = plot(x,phi[1:nc], label="fast neutrons")
+    plot!(x,phi[nc+1:end], label="slow neutrons")
+end
+reactor_with_reflector(0.1)
 savefig("docs/figs/ex3/bare.png")
