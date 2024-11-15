93e6c6394a4be057379e72d22b7ea646cc73660c
Caspar Gutsche <caspargutsche1@gmail.com>
2024-11-15 22:29:04 +0100

    second harmonic

--- src/ex3.jl
+++ src/ex3.jl
@@ -154,6 +154,13 @@ function reactor_with_reflector(dx; save = false, do_plot=false, verbose=false, 
     A = Matrix(A)
     F = Matrix(F)
     M = inv(A) * F
+    @show k = eigvals(M)[end-1]
+    phi = eigvecs(M)[:,end-1]
+    phi = real.(phi)
+    phi = phi ./ phi[nc ÷ 2]
+    p1 = plot(x,phi[1:nt], label="fast neutrons")
+    plot!(x,phi[nt+1:end], label="slow neutrons")
+    savefig("docs/figs/ex3/fist_harmonic_reflected.png")
     @show k = eigvals(M)[end]
     phi = eigvecs(M)[:,end]
     phi = real.(phi)
